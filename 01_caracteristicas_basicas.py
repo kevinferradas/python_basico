7f3f9139a9cc5e49e764b6def48069358e382937
Kevin <kferradas@unitru.edu.pe>
2025-01-29 17:11:28 +0100

    Ficheros iniciales

---
+++ 01_caracteristicas_basicas.py
@@ -0,0 +1,24 @@
+"""
+CARACTERÍSTICAS BÁSICAS DE PYTHON
+""" 
+
+# VARAIBLES
+# Una variable es un espacio en memoria para guardar datos 
+# por tanto, es un contenedor
+
+# Para crear una variable...
+# identificador = valor
+
+# Hay reglas para llamar a los identificadores = nombre de la variable
+# No se puede hacer:
+#    1variable (empezar por un número, pero después sí lo puede llevar)
+#    $variable (ni empezar ni contener símbolos especiales)
+# De estos errores nos avisará VSC
+
+# No debemos hacer  (no son exactamente errores):
+#     Contener caracteres fuera del idioma inglés, como ñ, ç, tildes, á,ö, etc
+#     Empezar por guión bajo (reservado para determinadas situaciones)
+
+# Qué debemos hacer:
+#     Nombrar a nuestras variables con palabras descriptivas
+#     Podemos usar más de una palabra separadas por un guión bajo (directiva PEP-8)
---
+++ 01_primer_python.py
@@ -0,0 +1,48 @@
+
+
+#Esto es un comentario de una línea
+numero_entero=2 # esto también funciona
+numero_decimal=4.5
+saludo="Buenas tardes"
+despedida='Adiós'
+
+declaration="I'm a developer"
+ejemplo_comilla_triple =""" 
+    estoy dentro de una 
+    comilla triple 
+"""
+
+"""
+Esto es un comentario
+
+"""
+
+verdad= True
+mentira= False 
+print(numero_entero)
+print("numero_entero es de tipo ", type(numero_entero))
+print(numero_decimal)
+print(ejemplo_comilla_triple)
+
+from datetime import datetime 
+print(datetime.now())
+
+suma = 1 + 3
+resta= 1-4
+multiplicacion = 4*2
+division = 10/5
+exponencial= 4** 0.5
+division_entera= 20//6
+modulo= 20%3
+
+
+print(modulo)
+
+texto1= "buenas"
+texto2= "noches"
+texto_final= texto1 +" "+ texto2
+print(texto_final)
+texto_final=f"{texto1} {texto2}"
+print(texto_final)
+texto_final = "{} {}".format(texto1,texto2)
+print(texto_final)
