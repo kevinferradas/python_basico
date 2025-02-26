fc32b193d04aa8279b442ab141c0a96122633a8b
kevin <kferradas@unitru.edu.pe>
2025-02-26 20:02:47 +0100

    Avanca 26/02

--- ejercicio_clases_biblioteca.py
+++ ejercicio_clases_biblioteca.py
@@ -42,16 +42,21 @@ class Biblioteca:
         self.lista_lectores=[]
         # Atributos adicional para controlar la cantidad de libros de la biblioteca.
         self.dic_libros={}
+        # Atributos adicional para controlar la reserva de libros de la biblioteca.
+        self.dic_reserva={}
 
     # Método para agregar lectores a la lista de lectores de la biblioteca
+    # lector será una instancia de la clase Lector.
     def agregar_lector (self, lector):
 
-        lector_datos = lector.nombre + " "+  lector.apellido
+        lector_datos = lector.nombre + " " +  lector.apellido
+        # registro de nuevos usuarios
         if lector_datos not in self.lista_lectores:
 
             self.lista_lectores.append(lector_datos) # agregamos a un nuevo lector
             return f"Se ha añadido correctamente al usuario {lector.nombre} {lector.apellido}.\n"
         
+        # si el usuario ya ha sido registrado antes...
         else:
             mensaje= f"El usuario {lector.nombre} {lector.apellido} ya ha sido registrado anteriormente."\
                      " Intente con otro nombre y apellido.\n"
@@ -62,33 +67,39 @@ class Biblioteca:
     def mostrar_lectores (self):
         mensaje=f"Los usuarios de la biblioteca {self.nombre}, son :\n"
         for lectores in self.lista_lectores:
-            mensaje += f"- {lectores}\n"
+            mensaje += f"- {lectores}\n\n"
         return mensaje
 
     # Método para agregar libros a la lista de libros de la biblioteca
+    # libro será una instancia de la clase Libro
+    # Se tendrá que especificar la cantidad de ejemplares de cierto libro.
     def agregar_libro(self,libro,ejemplares):
-
+        
+        # Registramos el libro por primera vez en el repositorio  
         if libro.titulo not in self.dic_libros:
-            self.dic_libros [libro.titulo] = ejemplares
+            self.dic_libros [libro.titulo] = ejemplares #se añade un par libro:ejemplar en el diccionario.
             mensaje = f"Se han añadido {ejemplares} ejemplares del libro '{libro.titulo}'"\
                       f"del autor {libro.nombre_autor} {libro.apellido_autor}.\n"
             return mensaje
+
+        # Agregamos ejemplares adicionales a un libro ya registrado.
         else:
-            self.dic_libros [libro.titulo] += ejemplares
+            self.dic_libros [libro.titulo] += ejemplares # se suma una cant. adicional de ejemplares a un libro.
             mensaje = f"Se han añadido {ejemplares} ejemplares adicionales del libro '{libro.titulo}'"\
                       f"del autor {libro.nombre_autor} {libro.apellido_autor}.\n"
             return mensaje
 
     
-    
+    # Método para buscar un libro en el repositorio de la biblioteca (diccionario)
+    # libro será una instancia de la clase Libro
     def buscar_libro (self,libro):
-        # buscamos el nombre del titulo en nuestro repositorio (diccionario)
 
             if libro.titulo in self.dic_libros:
                 return f"Disponemos de {self.dic_libros[libro.titulo]} ejemplares del libro '{libro.titulo}'.\n "
             else:
                 return f"No disponemos del libro '{libro.titulo}' del autor {libro.nombre_autor} {libro.apellido_autor}.\n"
-        
+
+    # Método para mostrar todos los libros del repositorio.    
     def mostrar_libros (self):
 
         mensaje=f"La biblioteca {self.nombre} cuenta con los siguientes libros :\n"
@@ -96,7 +107,14 @@ class Biblioteca:
             mensaje += f"- {libros} --> {self.dic_libros[libros]} ejemplares\n"
         return mensaje
 
-
+    # Método para reservar un libro de la biblioteca.
+    def reservar_libro (self,libro):
+        
+        if libro.titulo in self.dic_libros:
+            self.dic_reserva [libro.titulo] = self.dic_libros[libro.titulo]
+            return self.dic_reserva
+        else:
+            return f"No disponemos del libro '{libro.titulo}' del autor {libro.nombre_autor} {libro.apellido_autor}.\n"
 
 
 # Creamos instancias de la clase Lector
@@ -141,4 +159,5 @@ print(Bib_Agusti_Centelles.buscar_libro(libro4))
 # Mostramos los libros registrados en el repositorio 
 print(Bib_Agusti_Centelles.mostrar_libros())
     
-    
+# Reservamos un libro del repositorio
+print(Bib_Agusti_Centelles.reservar_libro(libro))
